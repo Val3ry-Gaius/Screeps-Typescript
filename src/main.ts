ea67837629590fb44c054d11255821a8b995f2f3
Val3ry-Gaius <love.is.violent@gmail.com>
2017-12-04 22:36:09 +0000

    Removing errant console.log

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ import { runTowers } from "./tower";
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
